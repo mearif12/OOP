32a65b5914b9c11e2b1eda881ddefd12d5df6393
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-02-22 13:19:38 +0600

    backslash characters & comments

---
+++ cout2.cpp
@@ -0,0 +1,17 @@
+//backslash character is a special character followed by backslash
+
+//backslash character=> \b,\t,\n,\',\",\\
+
+//std::endl is a manipulators to create new line
+
+/*
+  This is a 
+  multiline comment */
+
+#include<iostream>
+int main(){
+    std::cout<< "Hi, i am Arif \n";
+    std::cout<< "Roll \t 12"<<std::endl;
+    std::cout<< "Information & Communication Enginnering abbr as \'ICE\'";
+  return 0;  
+}

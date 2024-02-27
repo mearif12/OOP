f969f494b2266abfb64a8fef6d6ed9a2b563c4f8
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-02-27 23:55:04 +0600

    data-types & variable

---
+++ cout3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+   
+   //data-types:>char,int,float,bool
+   //string is a collection of char.so use array to multiple char
+   //variable naming:>data-type name = value ;
+   
+   char name[] ="Arif ";
+   int roll = 12;
+
+   std::cout<< name << std::endl;
+   std::cout<< roll << std::endl;
+
+ return 0;  
+}

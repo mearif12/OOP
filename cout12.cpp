892815f4c2393ead57026409024795f6b2facc8f
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-01 09:21:39 -0700

    array printing

---
+++ cout12.cpp
@@ -0,0 +1,24 @@
+//string & array needs : type var[];
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    int marks[5];
+    int x;
+
+   for(x=0; x<5; x++)
+   {
+       cout << "Marks for student " << x+1 << " = ";
+       cin >> marks[x];
+   }
+
+    // printing an array
+    for(x=0; x<5; x++)
+    {
+        cout << marks[x] << endl;
+    }
+
+    return 0;
+}

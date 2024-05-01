3120c5b41c8c082ac99ab3e5ed53379549ef4eb5
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-01 10:30:24 -0700

    function templates

---
+++ cout25.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template <class myTemplate, class myTemplate2>
+myTemplate add (myTemplate a, myTemplate2 b)
+{
+    return a+b;
+}
+
+int main()
+{
+    cout << add(10,20) << endl;
+    cout << add(10.5,20) << endl;
+
+    return 0;
+}

a1df6ea704c3eef0a9cd0b7d8fefda345ac3b799
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-07 09:56:02 -0700

    comments and escape sequences

---
+++ Java/out1.java
@@ -0,0 +1,9 @@
+//Printing output and comments
+/*
+   Escape sequences-> \t,\b,\n,\",\'
+*/
+public class out1{
+    public static void main(String[] args){
+        System.out.println("Hello \n This is \'Arif\' "+"\t"+12);
+    }
+}

60a4adf3dd09939598c1fd813478b81233e85c18
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-10 04:04:29 -0700

    data types and variables

---
+++ Java/out2.java
@@ -0,0 +1,13 @@
+class out2{
+  public static void main(String[] args){
+      double area;
+     final double pi = 3.14; //constant
+     int r = 5;
+     boolean logic = true;
+     String name = "Arif";
+     area = pi * r * r;
+     System.out.println(area);
+     System.out.println(logic);
+     System.out.println(name);
+  }
+}

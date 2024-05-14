f01bb73edd708e55fc6882b607af440e7757be3b
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-14 09:33:32 -0700

    unary operator

---
+++ Java/out5.java
@@ -0,0 +1,14 @@
+  /*
+  * Unary Operator
+  * Unary Plus, minus
+  */
+  class out5 {public static void main(String[] args) {
+      int x = 10;
+      System.out.println(+x);
+      System.out.println(-x);
+      System.out.println(x++);
+      System.out.println(++x);
+      System.out.println(x);
+      System.out.println(x--);
+      System.out.println(--x);
+  }}

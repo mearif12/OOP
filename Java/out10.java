8fae006f8dc1d285771c44f8dda37a28ba2a6120
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-15 03:26:13 -0700

    string methods

---
+++ Java/out10.java
@@ -0,0 +1,10 @@
+class out10{public static void main(String[] args){
+
+   //String Methods: var.length(),indexOf(),,toUpperCase(),toLowerCase()
+    
+    String name ="M E Arifur rahman";
+    System.out.println(name.length());
+    System.out.println(name.indexOf("Arifur"));
+    System.out.println(name.indexOf("r"));
+    System.out.println(name.toUpperCase());
+}}

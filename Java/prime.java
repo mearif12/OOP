6d9e01775d92ecef02811816465a1ede60a1f974
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-15 02:04:21 -0700

    prime numbers

---
+++ Java/prime.java
@@ -0,0 +1,28 @@
+    import java.util.Scanner;
+    public class prime {
+      public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+          System.out.print("Enter a positive integer: ");
+          int number = input.nextInt();
+          int count = 0;
+
+          if (number < 2) {
+            System.out.println("Not Prime number");
+          } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+              if (number % i == 0) {
+                count++;
+                break;
+              }
+            }
+            if (count == 0) {
+              System.out.println("Prime number");
+            } else {
+              System.out.println("Not Prime number");
+            }
+
+          }
+        } catch (Exception e) {
+          System.out.println("Invalid input");
+        }
+      }}

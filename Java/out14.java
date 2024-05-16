beb5b6a9d15e8e805ce03dbbd94916484b60cc0b
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-16 03:39:17 -0700

    class and object

---
+++ Java/out14.java
@@ -0,0 +1,12 @@
+// creating obects:=> className objectName = new className();
+class out14 {
+
+    String name = "ARIF";
+    int age = 12;
+    
+    public static void main(String[] args) {
+        out14 ex = new out14();
+        System.out.println("Name: " + ex.name);
+        System.out.println("Age: " + ex.age);
+    }
+}

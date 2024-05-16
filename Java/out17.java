ed1b2288e960c826a824c81162009a118e70d7b5
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-16 04:03:25 -0700

    abstraction

---
+++ Java/out17.java
@@ -0,0 +1,21 @@
+abstract class Fruits {
+    public abstract void taste();
+    public void eat() {
+        System.out.println("Let's eat Fruits.");
+    }
+}
+
+class Mango extends Fruits {
+    public void taste() {
+        System.out.println("Mango is sweet.");
+    }
+}
+
+
+public class Example3 {
+    public static void main(String[] args) {
+        Mango m = new Mango();
+        m.eat();
+        m.taste();
+    }
+}

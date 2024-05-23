410623348defa64a06962cb921f580aabac89a4d
M E Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2024-05-22 21:20:09 -0700

    1D array printing

--- cout12.cpp
+++ cout12.cpp
@@ -14,11 +14,26 @@ int main()
        cin >> marks[x];
    }
 
-    // printing an array
+    // printing an integer array
     for(x=0; x<5; x++)
     {
         cout << marks[x] << endl;
     }
 
+    //printing an string array
+        cout << "Enter the number of subjects: ";
+        cin >> n;
+        string sub[n];
+
+        cout << "Enter Subjects: " << endl;
+        for(int i = 0; i < n; i++) {
+            cin >> sub[i];
+        }
+
+        cout << "The entered subjects are: " << endl;
+        for(int i = 0; i < n; i++) {
+            cout << sub[i] << ",";
+        }
+        cout << endl;
     return 0;
 }

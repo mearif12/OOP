30a1068d8889fce2ab6e92bc15c244c77595eb14
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2025-12-08 08:40:23 -0800

    Loading image using GUI

---
+++ lab test/task15.java
@@ -0,0 +1,33 @@
+import java.awt.FlowLayout;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class ImageLoader {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Image Program");
+        frame.setLayout(new FlowLayout());
+
+        //Load original images
+        ImageIcon icon1 = new ImageIcon("ice.jpg");
+        ImageIcon icon2 = new ImageIcon("pust.jpg");
+
+        //Resize images (choose size you want)
+        Image scaled1 = icon1.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        Image scaled2 = icon2.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+
+        //Set resized images to labels
+        JLabel label1 = new JLabel(new ImageIcon(scaled1));
+        JLabel label2 = new JLabel(new ImageIcon(scaled2));
+
+        frame.add(label1);
+        frame.add(label2);
+
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}

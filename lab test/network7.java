ad4e8bc683af37ad644673ca9ced4ec65805a2fd
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2026-01-15 20:59:51 -0800

    Use of priority in threads

---
+++ lab test/network7.java
@@ -0,0 +1,65 @@
+// *** Use of priority in threads 
+
+class A extends Thread
+{
+    public void run()
+    {
+        System.out.println("threadA started");
+        for(int i=1; i<=4; i++)
+        {
+            System.out.println("\tFrom Thread A : i = " + i);
+        }
+        System.out.println("Exit from A ");
+    }
+}
+
+class B extends Thread
+{
+    public void run()
+    {
+        System.out.println("threadB started");
+        for(int j=1; j<=4; j++)
+        {
+            System.out.println("\tFrom Thread B : j = " + j);
+        }
+        System.out.println("Exit from B ");
+    }
+}
+
+class C extends Thread
+{
+    public void run()
+    {
+        System.out.println("threadC started");
+        for(int k=1; k<=4; k++)
+        {
+            System.out.println("\tFrom Thread C : k = " + k);
+        }
+        System.out.println("Exit from C ");
+    }
+}
+
+class ThreadPriority
+{
+    public static void main(String args[])
+    {
+        A threadA = new A();
+        B threadB = new B();
+        C threadC = new C();
+        
+        threadC.setPriority(Thread.MAX_PRIORITY);
+        threadB.setPriority(threadA.getPriority() + 1);
+        threadA.setPriority(Thread.MIN_PRIORITY);
+        
+        System.out.println("Start thread A");
+        threadA.start();
+        
+        System.out.println("Start thread B");
+        threadB.start();
+        
+        System.out.println("Start thread C");
+        threadC.start();
+        
+        System.out.println("End of main thread");
+    }
+}

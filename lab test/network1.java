300df7140cfd1c3ffcd00dd6644025d56c485dfe
ME Arifur Rahman <157901311+mearif12@users.noreply.github.com>
2026-01-16 11:00:06 -0800

    Loading image using GUI

--- lab test/network1.java
+++ lab test/network1.java
@@ -1,8 +1,5 @@
-import java.awt.FlowLayout;
-import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
 
 public class ImageLoader {
 
